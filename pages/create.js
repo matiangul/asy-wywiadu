028bedb29d9e8da3f31bff440fba617c5ad24633
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2020-04-13 16:27:45 +0200

    Adding words to new game

--- pages/create.js
+++ pages/create.js
@@ -1,27 +1,61 @@
 import Head from "next/head";
 import { component, global } from "../src/navigation/styles";
+import { useState } from "react";
 
-const Create = () => (
-  <div className="container">
-    <Head>
-      <title>Asy wywiadu - stwórz nową grę</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Create = () => {
+  const [words, setWords] = useState([]);
+  const [word, setWord] = useState("");
 
-    <main>
-      <h1 className="title">Hej Asie</h1>
-      <p className="description">Jesteś w trybie tworzenia nowej rozgrywki</p>
-    </main>
+  return (
+    <div className="container">
+      <Head>
+        <title>Asy wywiadu - stwórz nową grę</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <footer>
-      <a href="https://angulski.pl" target="_blank" rel="noopener noreferrer">
-        Autor Mateusz Angulski
-      </a>
-    </footer>
+      <main>
+        <h1 className="title">Hej Asie</h1>
+        <p className="description">Jesteś w trybie tworzenia nowej rozgrywki</p>
 
-    {component}
-    {global}
-  </div>
-);
+        <input
+          type="text"
+          name="word"
+          placeholder="Hasło"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setWord("");
+            if (word.trim().length === 0) {
+              return alert("Puste słowa nic dla mnie nie znaczą");
+            }
+            if (words.indexOf(word) !== -1) {
+              return alert("To już było");
+            }
+            setWords((list) => list.concat(word.trim()));
+          }}
+        >
+          Dodaj
+        </button>
+
+        <ul>
+          {words.map((w) => (
+            <li key={w}>{w}</li>
+          ))}
+        </ul>
+      </main>
+
+      <footer>
+        <a href="https://angulski.pl" target="_blank" rel="noopener noreferrer">
+          Autor Mateusz Angulski
+        </a>
+      </footer>
+
+      {component}
+      {global}
+    </div>
+  );
+};
 
 export default Create;
