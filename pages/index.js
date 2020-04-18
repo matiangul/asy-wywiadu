deed3ca889b9cf5ce4a35973ee1bb0514fd465fd
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2020-04-18 11:51:24 +0200

    Initialize firebase app

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,51 @@
 import Head from "next/head";
 import Link from "next/link";
 import { component, global } from "../src/navigation/styles";
+import firebase from "../src/firebase";
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Asy wywiadu</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = () => {
+  console.log(firebase);
 
-    <main>
-      <h1 className="title">Witam w grze Asy Wywiadu</h1>
-      <p className="description">
-        Możesz tutaj stworzyć nową grę lub dołączyć do istniejącej
-      </p>
+  return (
+    <div className="container">
+      <Head>
+        <title>Asy wywiadu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <div className="grid">
-        <Link href="/create">
-          <div className="card">
-            <h3>Rozpocznij nową grę</h3>
-            <p>Tutaj możesz stworzyć nową rozgrywkę dla swoich znajomych</p>
-          </div>
-        </Link>
+      <main>
+        <h1 className="title">Witam w grze Asy Wywiadu</h1>
+        <p className="description">
+          Możesz tutaj stworzyć nową grę lub dołączyć do istniejącej
+        </p>
 
-        <Link href="/join">
-          <div className="card">
-            <h3>Dołącz do gry</h3>
-            <p>Tutaj możesz dołączyć do już istniejącej rozgrywki</p>
-          </div>
-        </Link>
-      </div>
-    </main>
+        <div className="grid">
+          <Link href="/create">
+            <div className="card">
+              <h3>Rozpocznij nową grę</h3>
+              <p>Tutaj możesz stworzyć nową rozgrywkę dla swoich znajomych</p>
+            </div>
+          </Link>
 
-    <footer>
-      <a href="https://angulski.pl" target="_blank" rel="noopener noreferrer">
-        Autor Mateusz Angulski
-      </a>
-    </footer>
+          <Link href="/join">
+            <div className="card">
+              <h3>Dołącz do gry</h3>
+              <p>Tutaj możesz dołączyć do już istniejącej rozgrywki</p>
+            </div>
+          </Link>
+        </div>
+      </main>
 
-    {component}
-    {global}
-  </div>
-);
+      <footer>
+        <a href="https://angulski.pl" target="_blank" rel="noopener noreferrer">
+          Autor Mateusz Angulski
+        </a>
+      </footer>
+
+      {component}
+      {global}
+    </div>
+  );
+};
 
 export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// This import loads the firebase namespace along with all its type information.
+import * as firebase from "firebase/app";
+
+// These imports load individual services into the firebase namespace.
+import "firebase/auth";
+import "firebase/analytics";
+
+let app = null;
+
+if (firebase.apps.length > 0) {
+  app = firebase.apps[0];
+} else {
+  app = firebase.initializeApp({
+    apiKey: "AIzaSyB8hMpCCmpLt9EIuOTG0f1oRYXV28B7QqU",
+    authDomain: "asy-wywiadu.firebaseapp.com",
+    databaseURL: "https://asy-wywiadu.firebaseio.com",
+    projectId: "asy-wywiadu",
+    storageBucket: "asy-wywiadu.appspot.com",
+    messagingSenderId: "906845334929",
+    appId: "1:906845334929:web:a8a39cd9d05c80d4363e30",
+    measurementId: "G-WREW8047ZL",
+  });
+}
+
+export default app;
