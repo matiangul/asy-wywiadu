8ff52f0ec4f4fc09c7f147143ebb74a80ba645b2
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2020-11-05 19:34:42 +0100

    Use tailwindcss for styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,27 @@
-import * as Sentry from "@sentry/browser";
-import LogRocket from "logrocket";
-import App from "next/app";
+import * as Sentry from '@sentry/browser'
+import LogRocket from 'logrocket'
+import App from 'next/app'
+import '../src/css/tailwind.css'
 
-LogRocket.init("g02q0r/asy-wywiadu");
+LogRocket.init('g02q0r/asy-wywiadu')
 Sentry.init({
-  dsn:
-    "https://1d01be2d122a4ed0bbc013898e90df81@o383352.ingest.sentry.io/5213412",
-});
+  dsn: 'https://1d01be2d122a4ed0bbc013898e90df81@o383352.ingest.sentry.io/5213412',
+})
 
 class MyApp extends App {
   componentDidCatch(error, errorInfo) {
     LogRocket.getSessionURL((sessionURL) => {
       Sentry.withScope((scope) => {
-        scope.setExtra("sessionURL", sessionURL);
+        scope.setExtra('sessionURL', sessionURL)
         for (const key in errorInfo) {
-          scope.setExtra(key, errorInfo[key]);
+          scope.setExtra(key, errorInfo[key])
         }
-        Sentry.captureException(error);
-      });
-    });
+        Sentry.captureException(error)
+      })
+    })
 
-    super.componentDidCatch(error, errorInfo);
+    super.componentDidCatch(error, errorInfo)
   }
 }
 
-export default MyApp;
+export default MyApp
--- pages/join.tsx
+++ pages/join.tsx
@@ -1,149 +1,139 @@
-import * as Sentry from "@sentry/browser";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { component, global } from "../src/components/styles/welcome";
-import { TeamColor } from "../src/model/color";
-import { Player, Role } from "../src/model/player";
-import {
-  addGamePlayer,
-  loadGame,
-  loadPlayer,
-  watchGame,
-} from "../src/store/repository";
+import * as Sentry from '@sentry/browser'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { TeamColor } from '../src/model/color'
+import { Player, Role } from '../src/model/player'
+import { addGamePlayer, loadGame, loadPlayer, watchGame } from '../src/store/repository'
 
 export default () => {
-  const router = useRouter();
+  const router = useRouter()
 
-  const [gameName, choseGameName] = useState("");
-  useEffect(() => choseGameName((router.query.name as string) || ""), [
-    router.query.name,
-  ]);
+  const [gameName, choseGameName] = useState('')
+  useEffect(() => choseGameName((router.query.name as string) || ''), [router.query.name])
 
-  const [game, setGame] = useState(undefined);
+  const [game, setGame] = useState(undefined)
   useEffect(() => {
     if (!gameName) {
-      return;
+      return
     }
     loadGame(gameName)
       .then(setGame)
-      .then(() => watchGame(gameName, setGame));
-  }, [gameName]);
+      .then(() => watchGame(gameName, setGame))
+    // undefined when not found
+  }, [gameName])
 
   const defaultPlayerSettings = {
-    nick: "",
-    color: "red" as TeamColor,
-    role: "guesser" as Role,
-  };
-  const [player, setPlayer] = useState<Player>(defaultPlayerSettings);
+    nick: '',
+    color: 'red' as TeamColor,
+    role: 'guesser' as Role,
+  }
+  const [player, setPlayer] = useState<Player>(defaultPlayerSettings)
   useEffect(() => {
     if (!game) {
-      return;
+      return
     }
     loadPlayer(game).then((gamePlayer) => {
       if (gamePlayer) {
-        router.push(`/game/${game.name}`);
+        router.push(`/game/${game.name}`)
       }
-    });
-  }, [game]);
+    })
+  }, [game])
 
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Asy wywiadu - dołącz do gry</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">Hej Asie</h1>
-        <p className="description">Tutaj możesz dołączyć do istniejącej gry</p>
-        <form>
-          <input
-            type="text"
-            name="gameName"
-            placeholder="Nazwa rozgrywki"
-            value={gameName}
-            onChange={(e) => {
-              const name = e.target.value;
-              choseGameName(name);
-            }}
-          />
-          {game && (
-            <>
-              <p>Jak się zwiesz?</p>
-              <input
-                type="text"
-                name="playerNick"
-                placeholder="nick"
-                value={player.nick}
-                onChange={(e) => {
-                  const nick = e.target.value;
-                  setPlayer((player) => ({ ...player, nick }));
-                }}
-              />
-              <p>Wybierz drużynę</p>
-              <select
-                name="playerColor"
-                value={player.color}
-                onChange={(e) => {
-                  const color = e.target.value;
-                  setPlayer((player) => ({
-                    ...player,
-                    color: color as TeamColor,
-                  }));
-                }}
-              >
-                <option value="red">Czerwoni</option>
-                <option value="blue">Niebiescy</option>
-              </select>
-              <p>Wybierz swoją rolę w drużynie</p>
-              <select
-                name="playerRole"
-                value={player.role}
-                onChange={(e) => {
-                  const role = e.target.value;
-                  setPlayer((player) => ({ ...player, role: role as Role }));
-                }}
-              >
-                <option value="leader">Lider</option>
-                <option value="guesser">Zgadywacz</option>
-              </select>
+      <div className="flex justify-center items-center min-h-screen bg-yellow-300 flex-col">
+        <main>
+          <h1 className="text-center text-5xl leading-tight">Hej Asie</h1>
+          <p className="text-center text-2xl mt-6">Tutaj możesz dołączyć do istniejącej gry</p>
+          <form>
+            <input
+              type="text"
+              name="gameName"
+              placeholder="Nazwa rozgrywki"
+              value={gameName}
+              onChange={(e) => {
+                const name = e.target.value
+                choseGameName(name)
+              }}
+            />
+            {game && (
+              <>
+                <p>Jak się zwiesz?</p>
+                <input
+                  type="text"
+                  name="playerNick"
+                  placeholder="nick"
+                  value={player.nick}
+                  onChange={(e) => {
+                    const nick = e.target.value
+                    setPlayer((player) => ({ ...player, nick }))
+                  }}
+                />
+                <p>Wybierz drużynę</p>
+                <select
+                  name="playerColor"
+                  value={player.color}
+                  onChange={(e) => {
+                    const color = e.target.value
+                    setPlayer((player) => ({
+                      ...player,
+                      color: color as TeamColor,
+                    }))
+                  }}
+                >
+                  <option value="red">Czerwoni</option>
+                  <option value="blue">Niebiescy</option>
+                </select>
+                <p>Wybierz swoją rolę w drużynie</p>
+                <select
+                  name="playerRole"
+                  value={player.role}
+                  onChange={(e) => {
+                    const role = e.target.value
+                    setPlayer((player) => ({ ...player, role: role as Role }))
+                  }}
+                >
+                  <option value="leader">Lider</option>
+                  <option value="guesser">Zgadywacz</option>
+                </select>
 
-              {player.nick && player.color && player.role && (
-                <>
-                  <button
-                    type="submit"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      addGamePlayer(player, game)
-                        .then(() => router.push(`/game/${gameName}`))
-                        .catch((err) => {
-                          Sentry.captureException(err);
-                          alert(err.message);
-                        });
-                    }}
-                  >
-                    Dołącz
-                  </button>
-                </>
-              )}
-            </>
-          )}
-          {game === undefined && (
-            <p>Momencik, już szukam tej gry w internetach...</p>
-          )}
-          {game === null && <p>Nie ma takiej gry, sory :(</p>}
-        </form>
-      </main>
+                {player.nick && player.color && player.role && (
+                  <>
+                    <button
+                      type="submit"
+                      onClick={(e) => {
+                        e.preventDefault()
+                        addGamePlayer(player, game)
+                          .then(() => router.push(`/game/${gameName}`))
+                          .catch((err) => {
+                            Sentry.captureException(err)
+                            alert(err.message)
+                          })
+                      }}
+                    >
+                      Dołącz
+                    </button>
+                  </>
+                )}
+              </>
+            )}
+            {game === undefined && <p>Momencik, już szukam tej gry w internetach...</p>}
+            {game === null && <p>Nie ma takiej gry, sory :(</p>}
+          </form>
+        </main>
 
-      <footer>
-        <a href="https://angulski.pl" target="_blank" rel="noopener noreferrer">
-          Autor Mateusz Angulski
-        </a>
-      </footer>
-
-      {component}
-      {global}
+        <footer>
+          <a href="https://angulski.pl" target="_blank" rel="noopener noreferrer">
+            Autor Mateusz Angulski
+          </a>
+        </footer>
+      </div>
     </div>
-  );
-};
+  )
+}
